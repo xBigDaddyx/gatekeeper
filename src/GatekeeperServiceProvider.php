9387eca7261ebc2a3699019e699e4a86d7619fa9
xBigDaddyx <10373528+xBigDaddyx@users.noreply.github.com>
2025-03-16 14:15:24 +0000

    Fix styling

--- src/GatekeeperServiceProvider.php
+++ src/GatekeeperServiceProvider.php
@@ -18,143 +18,141 @@ use XBigDaddyx\Gatekeeper\Testing\TestsGatekeeper;
 
 class GatekeeperServiceProvider extends PackageServiceProvider
 {
-  public static string $name = 'gatekeeper';
+    public static string $name = 'gatekeeper';
 
-  public static string $viewNamespace = 'gatekeeper';
+    public static string $viewNamespace = 'gatekeeper';
 
-  public function configurePackage(Package $package): void
-  {
-    /*
-     * This class is a Package Service Provider
-     *
-     * More info: https://github.com/spatie/laravel-package-tools
+    public function configurePackage(Package $package): void
+    {
+        /*
+         * This class is a Package Service Provider
+         *
+         * More info: https://github.com/spatie/laravel-package-tools
+         */
+        $package->name(static::$name)
+            ->hasCommands($this->getCommands())
+            ->hasInstallCommand(function (InstallCommand $command) {
+                $command
+                    ->publishConfigFile()
+                    ->publishMigrations()
+                    ->askToRunMigrations()
+                    ->askToStarRepoOnGitHub('xbigdaddyx/gatekeeper');
+            });
+
+        $configFileName = $package->shortName();
+        if (file_exists($package->basePath('/../routes/web.php'))) {
+            $package->hasRoutes('web');
+        }
+        if (file_exists($package->basePath("/../config/{$configFileName}.php"))) {
+            $package->hasConfigFile();
+        }
+
+        if (file_exists($package->basePath('/../database/migrations'))) {
+            $package->hasMigrations($this->getMigrations());
+        }
+
+        if (file_exists($package->basePath('/../resources/lang'))) {
+            $package->hasTranslations();
+        }
+
+        if (file_exists($package->basePath('/../resources/views'))) {
+            $package->hasViews(static::$viewNamespace);
+        }
+    }
+
+    public function packageRegistered(): void {}
+
+    public function packageBooted(): void
+    {
+        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'gatekeeper');
+        // Asset Registration
+        FilamentAsset::register(
+            $this->getAssets(),
+            $this->getAssetPackageName()
+        );
+
+        FilamentAsset::registerScriptData(
+            $this->getScriptData(),
+            $this->getAssetPackageName()
+        );
+
+        // Icon Registration
+        FilamentIcon::register($this->getIcons());
+
+        // Handle Stubs
+        if (app()->runningInConsole()) {
+            foreach (app(Filesystem::class)->files(__DIR__ . '/../stubs/') as $file) {
+                $this->publishes([
+                    $file->getRealPath() => base_path("stubs/gatekeeper/{$file->getFilename()}"),
+                ], 'gatekeeper-stubs');
+            }
+        }
+
+        // Testing
+        // Testable::mixin(new TestsGatekeeper);
+    }
+
+    protected function getAssetPackageName(): ?string
+    {
+        return 'xbigdaddyx/gatekeeper';
+    }
+
+    /**
+     * @return array<Asset>
      */
-    $package->name(static::$name)
-      ->hasCommands($this->getCommands())
-      ->hasInstallCommand(function (InstallCommand $command) {
-        $command
-          ->publishConfigFile()
-          ->publishMigrations()
-          ->askToRunMigrations()
-          ->askToStarRepoOnGitHub('xbigdaddyx/gatekeeper');
-      });
-
-    $configFileName = $package->shortName();
-    if (file_exists($package->basePath('/../routes/web.php'))) {
-      $package->hasRoutes('web');
-    }
-    if (file_exists($package->basePath("/../config/{$configFileName}.php"))) {
-      $package->hasConfigFile();
+    protected function getAssets(): array
+    {
+        return [
+            // AlpineComponent::make('gatekeeper', __DIR__ . '/../resources/dist/components/gatekeeper.js'),
+            // Css::make('gatekeeper-styles', __DIR__ . '/../resources/dist/gatekeeper.css'),
+            // Js::make('gatekeeper-scripts', __DIR__ . '/../resources/dist/gatekeeper.js'),
+        ];
     }
 
-    if (file_exists($package->basePath('/../database/migrations'))) {
-      $package->hasMigrations($this->getMigrations());
+    /**
+     * @return array<class-string>
+     */
+    protected function getCommands(): array
+    {
+        return [
+            // GatekeeperCommand::class,
+        ];
     }
 
-    if (file_exists($package->basePath('/../resources/lang'))) {
-      $package->hasTranslations();
+    /**
+     * @return array<string>
+     */
+    protected function getIcons(): array
+    {
+        return [];
     }
 
-    if (file_exists($package->basePath('/../resources/views'))) {
-      $package->hasViews(static::$viewNamespace);
-    }
-  }
-
-  public function packageRegistered(): void
-  {
-  }
-
-  public function packageBooted(): void
-  {
-    $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'gatekeeper');
-    // Asset Registration
-    FilamentAsset::register(
-      $this->getAssets(),
-      $this->getAssetPackageName()
-    );
-
-    FilamentAsset::registerScriptData(
-      $this->getScriptData(),
-      $this->getAssetPackageName()
-    );
-
-    // Icon Registration
-    FilamentIcon::register($this->getIcons());
-
-    // Handle Stubs
-    if (app()->runningInConsole()) {
-      foreach (app(Filesystem::class)->files(__DIR__.'/../stubs/') as $file) {
-        $this->publishes([
-          $file->getRealPath() => base_path("stubs/gatekeeper/{$file->getFilename()}"),
-        ], 'gatekeeper-stubs');
-      }
+    /**
+     * @return array<string>
+     */
+    protected function getRoutes(): array
+    {
+        return [];
     }
 
-    // Testing
-    // Testable::mixin(new TestsGatekeeper);
-  }
+    /**
+     * @return array<string, mixed>
+     */
+    protected function getScriptData(): array
+    {
+        return [];
+    }
 
-  protected function getAssetPackageName(): ?string
-  {
-    return 'xbigdaddyx/gatekeeper';
-  }
-
-  /**
-   * @return array<Asset>
-   */
-  protected function getAssets(): array
-  {
-    return [
-      // AlpineComponent::make('gatekeeper', __DIR__ . '/../resources/dist/components/gatekeeper.js'),
-      // Css::make('gatekeeper-styles', __DIR__ . '/../resources/dist/gatekeeper.css'),
-      // Js::make('gatekeeper-scripts', __DIR__ . '/../resources/dist/gatekeeper.js'),
-    ];
-  }
-
-  /**
-   * @return array<class-string>
-   */
-  protected function getCommands(): array
-  {
-    return [
-      // GatekeeperCommand::class,
-    ];
-  }
-
-  /**
-   * @return array<string>
-   */
-  protected function getIcons(): array
-  {
-    return [];
-  }
-
-  /**
-   * @return array<string>
-   */
-  protected function getRoutes(): array
-  {
-    return [];
-  }
-
-  /**
-   * @return array<string, mixed>
-   */
-  protected function getScriptData(): array
-  {
-    return [];
-  }
-
-  /**
-   * @return array<string>
-   */
-  protected function getMigrations(): array
-  {
-    return [
-      '2023_01_01_000001_create_job_titles_table',
-      '2023_01_01_000002_create_approval_flows_table',
-      '2023_01_01_000003_create_approvals_table',
-      '2023_01_01_000004_add_jobtitle_id_columns_to_users_table',
-    ];
-  }
+    /**
+     * @return array<string>
+     */
+    protected function getMigrations(): array
+    {
+        return [
+            '2023_01_01_000001_create_job_titles_table',
+            '2023_01_01_000002_create_approval_flows_table',
+            '2023_01_01_000003_create_approvals_table',
+            '2023_01_01_000004_add_jobtitle_id_columns_to_users_table',
+        ];
+    }
 }
