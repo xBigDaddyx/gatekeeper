17012273e65821883be1ae5af1aa5ef9b41633be
xBigDaddyx <yussupcazzanova97@gmail.com>
2025-03-16 20:49:22 +0700

    updated

--- src/Filament/Tables/RejectAction.php
+++ src/Filament/Tables/RejectAction.php
@@ -9,49 +9,49 @@ use Illuminate\Database\Eloquent\Model;
 
 class RejectAction extends Action
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
+  protected function setUp(): void
+  {
+    parent::setUp();
 
-        $this->label(__('reject_action.label'))
-            ->icon('fluentui-dismiss-circle-20')
-            ->modalHeading(__('reject_action.modal_heading'))
-            ->modalDescription(__('reject_action.modal_description'))
-            ->modalSubmitActionLabel(__('reject_action.modal_submit'))
-            ->modalCancelActionLabel(__('reject_action.modal_cancel'))
-            ->requiresConfirmation()
-            ->form([
-                Textarea::make('reason')
-                    ->label(__('reject_action.reason_label'))
-                    ->required(),
-            ])
-            ->action(fn (Model $record, array $data) => $this->reject($record, $data['reason']));
+    $this->label(__('gatekeeper::gatekeeper.reject_action.label'))
+      ->icon('fluentui-dismiss-circle-20')
+      ->modalHeading(__('gatekeeper::gatekeeper.reject_action.modal_heading'))
+      ->modalDescription(__('gatekeeper::gatekeeper.reject_action.modal_description'))
+      ->modalSubmitActionLabel(__('gatekeeper::gatekeeper.reject_action.modal_submit'))
+      ->modalCancelActionLabel(__('gatekeeper::gatekeeper.reject_action.modal_cancel'))
+      ->requiresConfirmation()
+      ->form([
+        Textarea::make('reason')
+          ->label(__('gatekeeper::gatekeeper.reject_action.reason_label'))
+          ->required(),
+      ])
+      ->action(fn (Model $record, array $data) => $this->reject($record, $data['reason']));
+  }
+
+  protected function reject(Model $record, string $reason): void
+  {
+    try {
+      if (method_exists($record, 'reject')) {
+        $record->reject($reason);
+
+        Notification::make()
+          ->title(__('gatekeeper::gatekeeper.reject_action.success_title'))
+          ->success()
+          ->body(__('gatekeeper::gatekeeper.reject_action.success_message'))
+          ->send();
+      } else {
+        Notification::make()
+          ->title(__('gatekeeper::gatekeeper.reject_action.error_title'))
+          ->danger()
+          ->body(__('gatekeeper::gatekeeper.reject_action.error_message'))
+          ->send();
+      }
+    } catch (\Exception $e) {
+      Notification::make()
+        ->title(__('gatekeeper::gatekeeper.reject_action.exception_title'))
+        ->danger()
+        ->body(__('gatekeeper::gatekeeper.reject_action.exception_message', ['error' => $e->getMessage()]))
+        ->send();
     }
-
-    protected function reject(Model $record, string $reason): void
-    {
-        try {
-            if (method_exists($record, 'reject')) {
-                $record->reject($reason);
-
-                Notification::make()
-                    ->title(__('reject_action.success_title'))
-                    ->success()
-                    ->body(__('reject_action.success_message'))
-                    ->send();
-            } else {
-                Notification::make()
-                    ->title(__('reject_action.error_title'))
-                    ->danger()
-                    ->body(__('reject_action.error_message'))
-                    ->send();
-            }
-        } catch (\Exception $e) {
-            Notification::make()
-                ->title(__('reject_action.exception_title'))
-                ->danger()
-                ->body(__('reject_action.exception_message', ['error' => $e->getMessage()]))
-                ->send();
-        }
-    }
+  }
 }
