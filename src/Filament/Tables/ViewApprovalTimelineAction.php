0b82f29a62c45ecd53b9d5e13b9d0d02b807ce56
xBigDaddyx <yussupcazzanova97@gmail.com>
2025-03-16 21:39:55 +0700

    updated

--- src/Filament/Tables/ViewApprovalTimelineAction.php
+++ src/Filament/Tables/ViewApprovalTimelineAction.php
@@ -12,19 +12,19 @@ class ViewApprovalTimelineAction extends Action
     {
         parent::setUp();
 
-        $this->label(__('gatekeeper::gatekeeper.approval_timeline_action.label'))
-            ->slideOver()
-            ->modal()
-            ->modalIcon('fluentui-timeline-20')
-            ->icon('fluentui-timeline-20')
-            ->modalHeading(fn (Model $record) => __('gatekeeper::gatekeeper.approval_timeline_action.modal_heading', ['id' => $record->id]))
-            ->modalContent(fn (Model $record) => view('gatekeeper::gatekeeper.components.activity-timeline', [
-                'pendingApprovals' => $record->pendingApprovals()->latest()->get(),
-                'approvalHistory' => $record->approvalHistory()->latest()->get(),
-                'modelName' => class_basename($record),
-            ]))
-            ->modalWidth(MaxWidth::ExtraLarge)
-            ->modalSubmitAction(false)
-            ->modalCancelActionLabel(__('gatekeeper::gatekeeper.approval_timeline_action.modal_cancel_label'));
-    }
+    $this->label(__('gatekeeper::gatekeeper.approval_timeline_action.label'))
+      ->slideOver()
+      ->modal()
+      ->modalIcon('fluentui-timeline-20')
+      ->icon('fluentui-timeline-20')
+      ->modalHeading(fn (Model $record) => __('gatekeeper::gatekeeper.approval_timeline_action.modal_heading', ['id' => $record->id]))
+      ->modalContent(fn (Model $record) => view('gatekeeper::components.activity-timeline', [
+        'pendingApprovals' => $record->pendingApprovals()->latest()->get(),
+        'approvalHistory' => $record->approvalHistory()->latest()->get(),
+        'modelName' => class_basename($record),
+      ]))
+      ->modalWidth(MaxWidth::ExtraLarge)
+      ->modalSubmitAction(false)
+      ->modalCancelActionLabel(__('gatekeeper::gatekeeper.approval_timeline_action.modal_cancel_label'));
+  }
 }
